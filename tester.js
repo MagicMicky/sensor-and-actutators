27781e5e38ab9b066186033521840352aea0c272
Mickael Goubin <mickael@payname.fr>
2017-01-25 15:47:20 +0100

    subscribe to radiator from tester.js

--- tester.js
+++ tester.js
@@ -3,30 +3,35 @@ const {mqtt: mqttConfig} = require('./config')
 
 const MQTT_ADDR = 'mqtt://'+ mqttConfig.host + ':' + mqttConfig.port
 const SENSOR_TOPIC = '/readings/temperature'
+const RADIATOR_TOPIC = '/actuators/room-1'
 
 const mqttClient = mqtt.connect(MQTT_ADDR)
 
 const setSensorTemp = (sensor, value) => {
   const sensorInfo = {
-  "sensorID": sensor,
-  "type": "temperature",
-  "value": value
-}
+    "sensorID": sensor,
+    "type": "temperature",
+    "value": value
+  }
   mqttClient.publish(SENSOR_TOPIC, JSON.stringify(sensorInfo))
+  mqttClient.subscribe(RADIATOR_TOPIC)
 }
+
+mqttClient.on('message', (topic, msg) => {
+  if(topic === RADIATOR_TOPIC) {
+    console.log('[+] Valve openness to '+ JSON.parse(msg).level)
+  }
+})
 
 mqttClient.on('connect', () => {
   setTimeout(() => {
     setSensorTemp("sensor-1", 10)
   }, 1000)
-})
-mqttClient.on('connect', () => {
+
   setTimeout(() => {
     setSensorTemp("sensor-1", 15)
   }, 2000)
-})
 
-mqttClient.on('connect', () => {
   setTimeout(() => {
     setSensorTemp("sensor-2", 25)
   }, 2000)
